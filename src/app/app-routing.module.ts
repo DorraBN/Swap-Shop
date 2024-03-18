dedd7acc06f019674af6999cbd2d15125b0d21cb
DorraBN <dorransira18@gmail.com>
2024-03-18 17:01:58 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Importez le composant de connexion
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
   },
   {
-    path: '',
+    path: 'login', // Chemin pour le composant de connexion
+    component: LoginComponent, // Composant de connexion
+  },
+  {
+    path: '', // Redirection par défaut vers la page d'accueil
     redirectTo: 'home',
     pathMatch: 'full'
   },
