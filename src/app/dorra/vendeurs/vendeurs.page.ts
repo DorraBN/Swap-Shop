f65eb5a364fce3317cf0ea988c8c14aef364f790
DorraBN <dorransira18@gmail.com>
2024-04-21 15:38:54 +0100

    ajout django

--- src/app/dorra/vendeurs/vendeurs.page.ts
+++ src/app/dorra/vendeurs/vendeurs.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-vendeurs',
@@ -8,11 +10,31 @@ import { Router } from '@angular/router';
 })
 export class VendeursPage implements OnInit {
 
-  constructor( private router:Router) { }
+  constructor( private router:Router,public alertController: AlertController) { }
+
 
   ngOnInit() {
   }
+  async openHelpDialog() {
+    const alert = await this.alertController.create({
+      header: 'Détails',
+      message: `
+        <div class="details">
+          <img src="../../../assets/p1.jpeg" alt="" class="im7">
+          <h2>Nom & Prénom</h2>
+          <h4>Profession</h4>
+          <h4>Numéro de téléphone</h4>
+          <h4>Catégorie de produit</h4>
+        </div>
+      `,
+      buttons: ['OK']
+    });
 
+    await alert.present();
+  }
+
+
+  
 
   pro()
   {
